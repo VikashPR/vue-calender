090775aff2753fb4c4347fcc1946c4b2ca4b8368
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-06 11:39:14 +0530

    unknown commite

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueTextAreaAutoSize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextAreaAutoSize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCOzEL6JYYvcWHyciGEwt3-EiaYj-pn0jc",
+    authDomain: "calander-a201d.firebaseapp.com",
+    projectId: "calander-a201d",
+    storageBucket: "calander-a201d.appspot.com",
+    messagingSenderId: "243501604103",
+    appId: "1:243501604103:web:2ab6bef2cfa38e306879a2"
+});
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
